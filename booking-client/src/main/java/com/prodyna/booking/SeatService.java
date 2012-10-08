c75d1de093f82a44b2c3dd02373665909db184b4
Markus Konrad <mkonrad@prodyna.com>
2012-10-08 22:27:00 +0200

    Added @Comsumes and @Produces

--- booking-client/src/main/java/com/prodyna/booking/SeatService.java
+++ booking-client/src/main/java/com/prodyna/booking/SeatService.java
@@ -2,12 +2,17 @@ package com.prodyna.booking;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public interface SeatService {
 	
 	@POST
