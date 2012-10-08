9dcc260566b9d6b8ce6a3f8c0892e6af8e83e224
U-PRODYNA\mkonrad <mkonrad@PD-WG00097.prodyna.com>
2012-10-08 21:07:17 +0200

    Formatierung

--- booking-client/src/main/java/com/prodyna/booking/AircraftService.java
+++ booking-client/src/main/java/com/prodyna/booking/AircraftService.java
@@ -3,7 +3,10 @@ package com.prodyna.booking;
 import java.util.List;
 
 public interface AircraftService {
-    void create( String aid );
-    void delete( String aid );
-    List<String> list();
+    
+	void create( String aid );
+    
+	void delete( String aid );
+    
+	List<String> list();
 }
