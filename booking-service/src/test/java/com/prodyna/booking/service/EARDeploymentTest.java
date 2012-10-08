c8c39418a511fa6e114cf257364ee0b37b08558d
Darko Krizic <darko@krizic.net>
2012-10-08 22:50:52 +0200

    EARDeployment Test (must be changed to @RunAsClient)

---
+++ booking-service/src/test/java/com/prodyna/booking/service/EARDeploymentTest.java
@@ -0,0 +1,38 @@
+package com.prodyna.booking.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.prodyna.booking.AircraftService;
+
+@RunWith(Arquillian.class)
+public class EARDeploymentTest {
+
+	@Inject
+	private AircraftService as;
+
+	@Deployment
+	public static Archive<?> createDeployment() {
+		return ShrinkWrap.createFromZipFile(
+				EnterpriseArchive.class, new File(
+						"../booking-app/target/booking-app.ear"));
+	}
+
+	@Test
+	public void createAircraft() {
+		assertEquals(0, as.list().size());
+		as.create("D-EEFZ");
+		assertEquals(1, as.list().size());
+	}
+}
