5c8e82c50649ba0ab1d3212032797b4baa100a5c
Markus Konrad <mkonrad@prodyna.com>
2012-10-08 22:06:53 +0200

    Restified

--- booking-client/src/main/java/com/prodyna/booking/AircraftService.java
+++ booking-client/src/main/java/com/prodyna/booking/AircraftService.java
@@ -2,11 +2,23 @@ package com.prodyna.booking;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("aircraft")
 public interface AircraftService {
     
-	void create( String aid );
+	@POST
+	@Path("{aid}")
+	void create(@PathParam("aid") String aid );
     
-	void delete( String aid );
+	@DELETE
+	@Path("{aid}")
+	void delete(@PathParam("aid") String aid );
     
+	@GET
 	List<String> list();
 }
