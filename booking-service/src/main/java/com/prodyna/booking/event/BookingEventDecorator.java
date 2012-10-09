deb78c665709efcac05c85b7d30a096665204f63
Markus Konrad <mkonrad@prodyna.com>
2012-10-09 21:39:39 +0200

    Decorator fully implements interface, no longer abstract

--- booking-service/src/main/java/com/prodyna/booking/event/BookingEventDecorator.java
+++ booking-service/src/main/java/com/prodyna/booking/event/BookingEventDecorator.java
@@ -1,5 +1,7 @@
 package com.prodyna.booking.event;
 
+import java.util.List;
+
 import javax.decorator.Decorator;
 import javax.decorator.Delegate;
 import javax.enterprise.event.Event;
@@ -8,7 +10,7 @@ import javax.inject.Inject;
 import com.prodyna.booking.BookingService;
 
 @Decorator
-public abstract class BookingEventDecorator implements BookingService {
+public class BookingEventDecorator implements BookingService {
 
 	@Inject
 	@Delegate
@@ -23,5 +25,25 @@ public abstract class BookingEventDecorator implements BookingService {
 		String id = bs.book(fid, sid, pax);
 		be.fire(id);
 		return id;
+	}
+
+	@Override
+	public void cancel(String tid) {
+		bs.cancel(tid);
+	}
+
+	@Override
+	public List<String> list() {
+		return bs.list();
+	}
+
+	@Override
+	public String flightNumber(String tid) {
+		return bs.flightNumber(tid);
+	}
+
+	@Override
+	public String aircraft(String tid) {
+		return bs.aircraft(tid);
 	}
 }
