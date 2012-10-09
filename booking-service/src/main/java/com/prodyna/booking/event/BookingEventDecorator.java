f83b9f48e9cd0ccff022543c91aa3c56bbfa494a
Darko Krizic <darko@krizic.net>
2012-10-09 22:12:32 +0200

    Decorator is abstract again

--- booking-service/src/main/java/com/prodyna/booking/event/BookingEventDecorator.java
+++ booking-service/src/main/java/com/prodyna/booking/event/BookingEventDecorator.java
@@ -1,7 +1,5 @@
 package com.prodyna.booking.event;
 
-import java.util.List;
-
 import javax.decorator.Decorator;
 import javax.decorator.Delegate;
 import javax.enterprise.event.Event;
@@ -10,7 +8,7 @@ import javax.inject.Inject;
 import com.prodyna.booking.BookingService;
 
 @Decorator
-public class BookingEventDecorator implements BookingService {
+public abstract class BookingEventDecorator implements BookingService {
 
 	@Inject
 	@Delegate
@@ -25,25 +23,5 @@ public class BookingEventDecorator implements BookingService {
 		String id = bs.book(fid, sid, pax);
 		be.fire(id);
 		return id;
-	}
-
-	@Override
-	public void cancel(String tid) {
-		bs.cancel(tid);
-	}
-
-	@Override
-	public List<String> list() {
-		return bs.list();
-	}
-
-	@Override
-	public String flightNumber(String tid) {
-		return bs.flightNumber(tid);
-	}
-
-	@Override
-	public String aircraft(String tid) {
-		return bs.aircraft(tid);
 	}
 }
