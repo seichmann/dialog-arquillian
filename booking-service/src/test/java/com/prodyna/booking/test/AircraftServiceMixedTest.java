2efe38a52de93210a61fa492545dcb8ee81b3834
Markus Konrad <mkonrad@prodyna.com>
2012-10-10 17:42:11 +0200

    Mixed Test (DatabaseCleaner and REST)

---
+++ booking-service/src/test/java/com/prodyna/booking/test/AircraftServiceMixedTest.java
@@ -0,0 +1,56 @@
+package com.prodyna.booking.test;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.prodyna.booking.AircraftService;
+import com.prodyna.booking.test.util.DatabaseCleaner;
+
+@RunWith(Arquillian.class)
+public class AircraftServiceMixedTest {
+
+	@Deployment
+	public static Archive<?> createDeployment() {
+		final WebArchive archive = ShrinkWrap.create(WebArchive.class, "aircraft.war");
+		archive.addPackages(true, "com.prodyna.booking");
+		archive.addAsWebInfResource("META-INF/beans.xml", "beans.xml");
+		archive.addAsWebInfResource("META-INF/persistence.xml", "classes/META-INF/persistence.xml");
+		return archive;
+	}
+	
+	@Inject
+	private DatabaseCleaner dbc;
+	
+	private AircraftService as;
+	
+	@InSequence(1)
+	@Test
+	public void cleanDB() {
+		dbc.clean();
+	}
+	
+	@RunAsClient
+	@InSequence(2)
+	@Test
+	public void createAndCount() {
+		final String restEndpoint = "http://127.0.0.1:8080/aircraft/rest";
+		as = ProxyFactory.create(AircraftService.class, restEndpoint);
+		assertEquals(0,  as.list().size() );
+		as.create("D-ABVX");
+		as.create("D-AIRX");
+		assertEquals(2,  as.list().size() );
+	}
+	
+}
